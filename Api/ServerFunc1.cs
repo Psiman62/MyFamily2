ad3a2994e420afa40ba5cb860860aa883e9ff0b0
Psiman <simon@appsbyday.com>
2025-08-02 19:21:45 +1000

    added credits page

--- Api/ServerFunc1.cs
+++ Api/ServerFunc1.cs
@@ -29,25 +29,5 @@ namespace Api
 
             return response;
         }
-
-        private string GetSummary(int temp)
-        {
-            var summary = "Mild";
-
-            if (temp >= 32)
-            {
-                summary = "Hot";
-            }
-            else if (temp <= 16 && temp > 0)
-            {
-                summary = "Cold";
-            }
-            else if (temp <= 0)
-            {
-                summary = "Freezing";
-            }
-
-            return summary;
-        }
     }
 }
