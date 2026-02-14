ae451c54f3fc05b5178be858477af680889f4473
Psiman62 <psiman-hju@bytz.me>
2026-02-14 20:39:09 +1000

    drop net10.0 targets

--- Models/Models.cs
+++ Models/Models.cs
@@ -16,10 +16,10 @@ namespace Models
     [Entity(2)]
     public interface IMyTodoList : IEntityBase
     {
-        [Member(1)] IMyTodoItem? Item0 { get; set; }
-        [Member(2)] IMyTodoItem? Item1 { get; set; }
-        [Member(3)] IMyTodoItem? Item2 { get; set; }
-        [Member(4)] IMyTodoItem? Item3 { get; set; }
+        [Member(1)][Name("item0")] IMyTodoItem? Item0 { get; set; }
+        [Member(2)][Name("item1")] IMyTodoItem? Item1 { get; set; }
+        [Member(3)][Name("item2")] IMyTodoItem? Item2 { get; set; }
+        [Member(4)][Name("item3")] IMyTodoItem? Item3 { get; set; }
 
         IEnumerable<IMyTodoItem> Items { get; }
     }
@@ -27,10 +27,10 @@ namespace Models
     [Entity(3)]
     public interface IMyTodoItem : IEntityBase
     {
-        [Member(1)] int Id { get; set; }
-        [Member(2)] bool Started { get; set; }
-        [Member(3)] bool Completed { get; set; }
-        [Member(4)] string Description { get; set; }
+        [Member(1)][Name("id")] int Id { get; set; }
+        [Member(2)][Name("started")] bool Started { get; set; }
+        [Member(3)][Name("completed")] bool Completed { get; set; }
+        [Member(4)][Name("description")] string? Description { get; set; }
 
         string State { get; }
     }
